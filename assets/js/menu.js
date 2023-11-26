5d062e493d3292244244b3e78f82ca4e6022f332
Natalia Repkina <pn.88@mail.ru>
2023-11-26 12:17:40 +0300

    feat: add function create cards

---
+++ assets/js/menu.js
@@ -0,0 +1,44 @@
+const cardsList = document.querySelector("cards-list");
+
+//получаем с сервера json
+async function createComponent() {
+  let response = await fetch("assets/products.json");
+  const productsJSON = await response.json();
+
+  addCards(cardsList, productsJSON);
+}
+createComponent();
+
+function createCard(product) {
+  const name = product.name;
+  const description = product.description;
+  const price = product.price;
+  const category = product.category;
+
+  const urlFolderImages = "assets/images/menu";
+  const image =
+    urlFolderImages + product.name.toLowerCase().split(" ").join("-") + ".jpg";
+
+  const card = document.createElement("li");
+  card.classeList.add("card", "cards-list__card");
+  card.dataset.category = category;
+
+  const content = `<div class="card__info">
+  <div class="card__img">
+    <img src=${image} alt="${name} foto" >
+  </div>
+  <div class="card__title">${name}</div>
+  <div class="card__desc">${description}</div>
+</div>
+<div class="card__price">$${price}</div>`;
+
+  card.insertAdjacentHTML("afterbegine", content);
+
+  return card;
+}
+
+function addCards(place, productsJSON) {
+  productsJSON.forEach((product) => {
+    place.append(createCard(product));
+  });
+}
