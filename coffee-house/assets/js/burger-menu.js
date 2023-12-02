5e11321ca3182414d5c0e35618dc400c0a8adff9
Natalia Repkina <pn.88@mail.ru>
2023-12-02 15:06:50 +0300

    feat: : add change in the number of cards to the page for mobile devices

--- coffee-house/assets/js/burger-menu.js
+++ coffee-house/assets/js/burger-menu.js
@@ -1,8 +1,3 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const ico = document.querySelector(".burger-menu__ico");
-const menu = document.getElementById("burger-nav");
-const closeButton = burgerMenu.querySelector(".close");
-
 function openMenu(event) {
   if (event.target.closest(".burger-menu__ico") || event.target.closest("a")) {
     burgerMenu.classList.toggle("active");
---
+++ coffee-house/assets/js/const.js
@@ -0,0 +1,9 @@
+//page menu
+const cardsListWrapper = document.querySelector(".cards-list");
+const buttonsMenuSwitcher = document.querySelectorAll(".menu-switcher__button");
+const buttonLoadMore = document.querySelector(".button_load-more");
+//burger-menu
+const burgerMenu = document.querySelector(".burger-menu");
+const ico = document.querySelector(".burger-menu__ico");
+const menu = document.getElementById("burger-nav");
+const closeButton = burgerMenu.querySelector(".close");
