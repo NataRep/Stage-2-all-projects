826196195ea390842d0a4f099a72aa9dfead8b33
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2024-03-15 10:16:35 +0300

    Revert "feat: RSS-PZ-09 implement dynamic word card sizing (#127)" (#128)
    This reverts commit 7b7ed0f117195337965c335d04682b58648fe848.

--- rss-puzzle/.husky/validate-branch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-branch_name=$(git rev-parse --abbrev-ref HEAD)
-
-if ! echo "$branch_name" | grep -E "^(master|main){1}$|^(feat|fix|refactor)/RSS-PZ-.+$" ; then
-    echo "Invalid branch name. Branch names must follow the defined rules."
-    exit 1
-fi
--- rss-puzzle/src/index.ts
+++
@@ -1,6 +0,0 @@
-import './style.scss';
-import App from './app/app';
-
-const app: App = new App();
-app.start();
-export default app;
