bceebba94933002aad890407d9be587dcf7fa4d5
Natalia Repkina <pn.88@mail.ru>
2024-03-26 11:23:46 +0300

    refactor: delete test code from index

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -12,7 +12,8 @@ class App {
 
   public async start() {
     //получаем данные объект: { cars: data, totalCount };:
-    //const carsData = await Api.getCars(1, 7);
+    const carsData = await Api.getCars(1, 7);
+    console.log(carsData);
     //рисуем страницу гаража:
     //инпуты значение берем из state
     //общее количество машин из полученного объекта
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,12 +1,5 @@
 import Api from './app/api';
+import App from './app/app';
 
-//api.createCar('ford nata', '#000000').then((result) => console.log(result));
-Api.getCars().then((result) => console.log(result));
-
-Api.updateWinner(1, 2, 4.2)
-  .then(() => Api.getWinners())
-  .then((result) => console.log(result));
-//  .startOrStopCar(2, 'started')
-//  .then((result) => console.log(result))
-//  .then(() => api.startOrStopCar(2, 'stopped'))
-//  .then((result) => console.log(result));
+export const app = new App();
+app.start();
