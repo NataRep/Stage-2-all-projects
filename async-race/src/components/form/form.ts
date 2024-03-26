d099d38ad061f10c12d93a2085adbf7b7a1d865d
Natalia Repkina <pn.88@mail.ru>
2024-03-26 12:03:34 +0300

    feat: add component Form

---
+++ async-race/src/components/form/form.ts
@@ -0,0 +1,43 @@
+import './form.scss';
+import Button from '../button/button';
+import { HexColor } from '../../utils/types';
+
+class Form {
+  static create(
+    value: string,
+    color: HexColor,
+    className: string,
+    buttonText: string,
+    buttonClassList: string[],
+    buttonHandler: (event: Event) => void
+  ): HTMLDivElement {
+    const wrapper: HTMLDivElement = document.createElement('div');
+    wrapper.className = 'form-wrapper';
+
+    const form: HTMLFormElement = document.createElement('form');
+    form.className = `form_${className} form`;
+
+    const inputText: HTMLInputElement = document.createElement('input');
+    inputText.className = 'form__input input_text';
+    inputText.type = 'text';
+    inputText.placeholder = 'Mercedes-Benz';
+    inputText.maxLength = 20;
+    inputText.pattern = '^[a-zA-Z0-9]+$';
+    inputText.value = value;
+
+    const inputColor: HTMLInputElement = document.createElement('input');
+    inputColor.className = 'form__input input_color';
+    inputColor.type = 'color';
+    inputColor.value = color;
+
+    const button = Button.create(buttonText, buttonClassList, buttonHandler);
+
+    form.append(inputText);
+    form.append(inputColor);
+    form.append(button);
+    wrapper.append(form);
+    return wrapper;
+  }
+}
+
+export default Form;
