4f6121b0cc092552ed0c7d93b584da0fdc12b9fb
Natalia Repkina <pn.88@mail.ru>
2024-04-11 16:45:11 +0300

    feat: add logotype on the login page

---
+++ fun-chat/src/components/logotype/logotype.ts
@@ -0,0 +1,7 @@
+import './logotype.scss';
+
+export function createLogotype() {
+  const logotype = document.createElement('div');
+  logotype.className = 'logotype';
+  return logotype;
+}
--- fun-chat/src/pages/login/login-page.ts
+++ fun-chat/src/pages/login/login-page.ts
@@ -1,12 +1,15 @@
 import App from '../../app/app';
 import LoginForm from '../../components/form/login-form';
+import { createLogotype } from '../../components/logotype/logotype';
 import Page from '../page';
 import './login-page.scss';
 
 export default class LoginPage extends Page {
   constructor(app: App) {
     super();
+    const logotype = createLogotype();
     const loginForm = new LoginForm(app).create();
+    this.mainContent.append(logotype);
     this.mainContent.append(loginForm);
   }
 }
