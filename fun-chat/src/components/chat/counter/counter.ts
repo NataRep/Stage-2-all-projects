eb02df98ccd97b835e6cb559178b7b0e23659c51
Natalia Repkina <pn.88@mail.ru>
2024-04-19 17:16:50 +0300

    feat: add style for counter

--- fun-chat/src/components/chat/counter/counter.ts
+++ fun-chat/src/components/chat/counter/counter.ts
@@ -10,7 +10,7 @@ export default class Counter {
 
   createEl(): HTMLElement {
     const elem: HTMLElement = document.createElement('div');
-    elem.className = 'users-list__user-counter';
+    elem.className = 'users-list__user-counter counter_zero';
     if (this.count) {
       elem.innerHTML = String(this.count);
     }
@@ -20,11 +20,13 @@ export default class Counter {
   public increase() {
     this.count += 1;
     this.elem.innerHTML = String(this.count);
+    if (this.elem.classList.contains('counter_zero')) this.elem.classList.remove('counter_zero');
   }
 
   public clear() {
     console.log('очищаю');
     this.count = 0;
     this.elem.innerHTML = '';
+    this.elem.classList.add('counter_zero');
   }
 }
