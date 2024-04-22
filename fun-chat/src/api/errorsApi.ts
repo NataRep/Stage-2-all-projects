cb53d7d380ee496fae1bb676e8f2ca69eec9fe20
Natalia Repkina <pn.88@mail.ru>
2024-04-22 10:10:45 +0300

    fix: change catching error from server

--- fun-chat/src/api/errorsApi.ts
+++ fun-chat/src/api/errorsApi.ts
@@ -2,22 +2,13 @@ import App from '../app/app';
 import PopUp from '../components/popUp/popUp';
 import { ResponseServer } from '../utils/interfaces.ts/interfaces';
 
-const errorText: { [key: string]: string } = {
-  USER_ERROR:
-    'a user with this login is already authorized' ||
-    'incorrect password' ||
-    'the user with the specified login does not exist' ||
-    'incorrect password' ||
-    'the user was not authorized',
-
-  //MESSAGE_SEND_TO_YOURSELF = 'sender and recipient logins are the same',
-  //MESSAGE_LOGIN_DOESNT_EXIST = 'the user with the specified login does not exist',
-  //MESSAGE_SENDER_ERROR = 'user not registered or not logged',
-
-  // MESSAGE_HISTORY_SEND_TO_YOURSELF = 'sender and recipient logins are the same',
-  //MESSAGE_HISTORY_LOGIN_DOESNT_EXIST = 'the user with the specified login does not exist',
-  //MESSAGE_HISTORY_SENDER_ERROR = 'user not registered or not logged',
-};
+const userError: string[] = [
+  'a user with this login is already authorized',
+  'incorrect password',
+  'the user with the specified login does not exist',
+  'incorrect password',
+  'the user was not authorized',
+];
 
 export default class ErrorsFromResponses {
   message: ResponseServer;
@@ -32,19 +23,6 @@ export default class ErrorsFromResponses {
     this.error = errorMessage.payload.error;
   }
 
-  private checkTypeError(text: string): string {
-    let result: string;
-    for (const key in errorText) {
-      if (errorText[key] === text) {
-        result = key;
-      }
-    }
-    if (!result) {
-      throw new Error(this.error);
-    }
-    return result;
-  }
-
   private handlerUserError(app: App) {
     if (window.location.pathname != app.router.urlPath.LOGIN) {
       app.logout();
@@ -52,9 +30,11 @@ export default class ErrorsFromResponses {
   }
 
   public catchError(app: App) {
-    const typeError = this.checkTypeError(this.message.payload.error);
-    if (typeError === 'USER_ERROR') this.handlerUserError(app);
-    this.displayError(this.message.payload.error);
+    const errorText = this.message.payload.error;
+    if (typeof errorText === 'string') {
+      if (userError.indexOf(errorText) > 0) this.handlerUserError(app);
+      this.displayError(this.message.payload.error);
+    }
   }
 
   public displayError(text: string) {
